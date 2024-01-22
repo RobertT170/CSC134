924cf7070cb14e8a9074224b83c88e16371fe5ab
RobertT170 <157392630+RobertT170@users.noreply.github.com>
2024-01-22 16:17:00 +0000

    Completed M1T2

---
+++ M1T2_Tunoa.cpp
@@ -0,0 +1,24 @@
+// CSC 134
+// M1T2
+// Robert Tunoa
+// 1/22/24
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Introduce yourself
+    cout << "Hello World!" << endl;
+    cout << "My name is Robert Tunoa" << "\n";
+    cout << "My hobbies are playing videogames and workingout" << endl;
+
+    // Ask a question
+    string name;
+    cout << "What's your name" << endl;
+    cin >> name;
+    
+    cout << "Nice to meet you, " << name << endl;
+
+    cout << endl << endl;
+    return 0;
+}
