91378ee8887cbc27704c2e6a48ad2d404a2e0c02
RobertT170 <157392630+RobertT170@users.noreply.github.com>
2024-02-12 21:54:20 +0000

    M2HW1 Finished 3

---
+++ M2HW3_Tunoa.cpp
@@ -0,0 +1,26 @@
+/*
+M2HW1
+Question 3
+Cheering Program
+Robert Tunoa
+2/12/24
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string letsGo = "Let's go ";
+    string school = "FTCC";
+    string team = "Trojans";
+    string cheerOne = letsGo + school;
+    string cheerTwo = letsGo + team;
+
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerTwo << endl;
+
+    return 0;
+}
