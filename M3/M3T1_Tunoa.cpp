9f5f8ac9d2014a916dd6da469716816446df07b1
RobertT170 <157392630+RobertT170@users.noreply.github.com>
2024-02-14 16:25:59 +0000

    M3T1 Finished

--- M3/M3T1_Tunoa.cpp
+++ M3/M3T1_Tunoa.cpp
@@ -1,5 +1,48 @@
-// Use as Starter
 // Csc 134
 // M3T1
 // Robert Tunoa
 // 2/14/24
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare vaiables
+    double lengthA, widthA;
+    double areaA;
+    // Ask for length and width of first rectangle
+    cout << "What is the lengthA? ";
+    cin >> lengthA; 
+    cout << "What is the widthA? ";
+    cin >> widthA;
+    // Calculate the areas
+    areaA = lengthA * widthA;
+    //Print the areas
+    cout << "The area is " << areaA << endl;
+
+ // Declare vaiables B
+    double lengthB, widthB;
+    double areaB;
+    // Ask for length and width of first rectangle
+    cout << "What is the lengthB? ";
+    cin >> lengthB; 
+    cout << "What is the widthB? ";
+    cin >> widthB;
+    // Calculate the areas
+    areaB = lengthB * widthB;
+    //Print the areas
+    cout << "The area is " << areaB << endl;
+    if (areaA > areaB) {
+        cout << "Rectangle A is bigger than Rectangle B";
+    } else {
+         cout << "Rectangle B is bigger than Rectangle A" << endl;
+    }
+    if (areaA == areaB) {
+        cout << "Rectangle A is equal to Rectangle B";
+    } else {
+        cout << "Rectangle A is not equal to Rectangle B";
+    
+    }
+
+    return 0;
+    }
