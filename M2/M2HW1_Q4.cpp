00e13add8d7db41103db5a0c2bf32020dd504b90
RobertT170 <157392630+RobertT170@users.noreply.github.com>
2024-02-16 17:05:44 +0000

    M2HW1_Q4 Finished

--- M2/M2HW1_Q4.cpp
+++ M2/M2HW1_Q4.cpp
@@ -5,67 +5,40 @@ Banking
 Robert Tunoa
 2/12/24
 */
-#include <iostream>
 #include <string>
-#include <cstdlib>
-#include <ctime>
+#include <iostream>
 #include <iomanip>
-int setprecision(2);
 using namespace std;
 
-class BankAccount {
-private:
-    string name;
-    int accountNumber;
-    double balance;
-    
-public:
-    BankAccount(string n, double bal) {
-        name = n;
-        balance = bal;
-        srand(time(0)); // Seed for random number generation
-        accountNumber = rand() % 10000 + 1000; // Generating a random 4-digit account number
-    }
-
-    void deposit(double amount) {
-        balance += amount;
-    }
-
-    void withdraw(double amount) {
-        if (balance >= amount) {
-            balance -= amount;
-        } else {
-            cout << "Insufficient funds!" << endl;
-        }
-    }
-
-    void displayInfo() {
-        cout << "Name on the account: " << name << endl;
-        cout << "Account number: " << accountNumber << endl;
-        cout << "Final account balance: $" << balance << endl;
-    }
-};
-
 int main() {
-    string name;
-    double startingBalance, deposit, withdrawal;
+// Declare Variables
+string name;
+double starting_balance;
+double amount_of_deposit;
+double amount_of_withdrawal; 
+double final_acc_balance;
+double accountnum = 983823040;
+cout << fixed << setprecision(2);
+// Ask for name, starting balance, amount of deposit, withdrawal
+cout << "Greetings, What's your name" << endl;
+cin >> name;
+cout << "What's your starting balance" << endl;
+cout << "$";
+cin >> starting_balance;
+cout << "What's your amount of deposit" << endl;
+cout << "$";
+cin >> amount_of_deposit;
+cout << "What's your amount of withdrawal" << endl;
+cout << "$";
+cin >> amount_of_withdrawal;
 
-    cout << "Enter your name: ";
-    getline(cin, name);
+// Do calculations
+final_acc_balance = starting_balance + amount_of_deposit - amount_of_withdrawal;
 
-    cout << "Enter starting account balance: $";
-    cin >> startingBalance;
-
-    cout << "Enter amount of deposit: $";
-    cin >> deposit;
-
-    cout << "Enter amount of withdrawal: $";
-    cin >> withdrawal;
-
-    BankAccount account(name, startingBalance);
-    account.deposit(deposit);
-    account.withdraw(withdrawal);
-    account.displayInfo();
+// Display account name, number, final balance
+cout << "Account name: " << name << endl;
+cout << "# " << accountnum << endl;
+cout << "Account balance: $" << final_acc_balance << endl;
 
     return 0;
 }
