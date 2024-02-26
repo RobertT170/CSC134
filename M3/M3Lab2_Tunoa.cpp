d9719df8390e7d5bac3e4d2b857716e93f0be642
RobertT170 <157392630+RobertT170@users.noreply.github.com>
2024-02-26 15:48:28 +0000

    M3Lab2 Finished

--- M3/M3Lab2_Tunoa.cpp
+++ M3/M3Lab2_Tunoa.cpp
@@ -10,15 +10,24 @@ int main() {
 
 // Declare Variables
     int grade;
+    int arr[100];
 
 // Ask question
-    cout << "What's your grade? " << endl;
+    cout << "Enter your numerical grade: " << endl;
     cin >> grade;
-
-    
-
-
-
+    if (grade >= 90 && grade <= 100) {
+        cout << "Your letter grade is: A " << endl;
+    } else if ( grade >= 80 && grade <= 89) {
+        cout << "Your letter grade is: B " << endl;
+    } else if ( grade >= 70 && grade <= 79) {
+        cout << "Your letter grade is: C " << endl;
+    } else if ( grade >= 60 && grade <= 69) {
+        cout << "Your letter grade is: D " << endl;
+    } else if ( grade >= 0 && grade <= 59) {
+        cout << "Your letter grade is: F " << endl;
+    } else if (grade < 0) {
+        cout << "This is a invaild number" << endl;
+    }
 
 
     return 0;
