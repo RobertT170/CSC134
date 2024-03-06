a9036ce4c588833d741da0bd4f1cbb77e5534c5d
RobertT170 <157392630+RobertT170@users.noreply.github.com>
2024-03-06 10:59:00 -0500

    Add files via upload

---
+++ M4LAB1_Tunoa.cpp
@@ -0,0 +1,35 @@
+// CSC 134
+// M4LAB1
+// Robert Tunoa
+// 3/6/24
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "M4LAB1" << endl;
+    //TODO ask user for width and height
+    int width = 6;
+    int height = 5;
+
+    // first a row
+    for (int i=0; i < width; i++) {
+        cout << "# ";
+    }
+    cout << endl << endl;
+    // next, a column
+     for (int i=0; i < height; i++) {
+        cout << "#";
+    }
+    cout << endl << endl;
+    // put it together
+    for (int i=0; i < height; i++) {
+        for (int j=0; j < width; j++) {
+            cout << "# ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
